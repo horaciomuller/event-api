09a6ac91e46ef4f4cb99b6c1cce3771635762d52
Fiasco <contato@java10x.dev>
2024-12-18 16:48:37 +0000

    refactor: Remove lombok and impl. constructors and setters mannually

--- src/main/java/dev/java10x/EnvetClean/infrastructure/mapper/EventoDtoMapper.java
+++ src/main/java/dev/java10x/EnvetClean/infrastructure/mapper/EventoDtoMapper.java
@@ -6,33 +6,33 @@ import org.springframework.stereotype.Component;
 @Component
 public class EventoDtoMapper {
 
-        public EventoDto toDto(Evento evento) {
-            return new EventoDto(
-                evento.id(),
-                evento.nome(),
-                evento.descricao(),
-                evento.identificador(),
-                evento.dataInicio(),
-                evento.dataFim(),
-                evento.localEvento(),
-                evento.capacidade(),
-                evento.organizador(),
-                evento.tipo()
-            );
-        }
+    public EventoDto toDto(Evento evento) {
+        return new EventoDto(
+            evento.id(),
+            evento.nome(),
+            evento.descricao(),
+            evento.identificador(),
+            evento.dataInicio(),
+            evento.dataFim(),
+            evento.localEvento(),
+            evento.capacidade(),
+            evento.organizador(),
+            evento.tipo()
+        );
+    }
 
-        public Evento toDomain(EventoDto eventoDto) {
-            return new Evento(
-                eventoDto.id(),
-                eventoDto.nome(),
-                eventoDto.descricao(),
-                eventoDto.identificador(),
-                eventoDto.dataInicio(),
-                eventoDto.dataFim(),
-                eventoDto.localEvento(),
-                eventoDto.capacidade(),
-                eventoDto.organizador(),
-                eventoDto.tipo()
-            );
-        }
+    public Evento toDomain(EventoDto eventoDto) {
+        return new Evento(
+            eventoDto.id(),
+            eventoDto.nome(),
+            eventoDto.descricao(),
+            eventoDto.identificador(),
+            eventoDto.dataInicio(),
+            eventoDto.dataFim(),
+            eventoDto.localEvento(),
+            eventoDto.capacidade(),
+            eventoDto.organizador(),
+            eventoDto.tipo()
+        );
+    }
     }
--- src/main/java/dev/java10x/EnvetClean/infrastructure/persistence/EventoEntity.java
+++ src/main/java/dev/java10x/EnvetClean/infrastructure/persistence/EventoEntity.java
@@ -1,15 +1,8 @@
 package dev.java10x.EnvetClean.infrastructure.persistence;
 import dev.java10x.EnvetClean.core.enuns.TipoEvento;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Entity
 @Table(name = "Eventos")
 public class EventoEntity {
@@ -27,4 +20,103 @@ public class EventoEntity {
     private String organizador;
     @Enumerated(EnumType.STRING)
     private TipoEvento tipo;
+
+    public EventoEntity() {
+    }
+
+    public EventoEntity(Long id, String nome, String descricao, String identificador, LocalDateTime dataInicio, LocalDateTime dataFim, String localEvento, Integer capacidade, String organizador, TipoEvento tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.identificador = identificador;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.localEvento = localEvento;
+        this.capacidade = capacidade;
+        this.organizador = organizador;
+        this.tipo = tipo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public LocalDateTime getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDateTime dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDateTime getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDateTime dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public String getLocalEvento() {
+        return localEvento;
+    }
+
+    public void setLocalEvento(String localEvento) {
+        this.localEvento = localEvento;
+    }
+
+    public Integer getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(Integer capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public String getOrganizador() {
+        return organizador;
+    }
+
+    public void setOrganizador(String organizador) {
+        this.organizador = organizador;
+    }
+
+    public TipoEvento getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoEvento tipo) {
+        this.tipo = tipo;
+    }
 }
+
+
+
