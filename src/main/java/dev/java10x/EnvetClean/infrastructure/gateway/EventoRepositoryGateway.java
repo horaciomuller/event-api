9111b3515f8f683eee35e9c79914be7b397a5e7e
Fiasco <contato@java10x.dev>
2024-12-18 13:12:44 +0000

    chore: implements EventoGateway into infra layar

---
+++ src/main/java/dev/java10x/EnvetClean/infrastructure/gateway/EventoRepositoryGateway.java
@@ -0,0 +1,5 @@
+package dev.java10x.EnvetClean.infrastructure.gateway;
+import dev.java10x.EnvetClean.core.gateway.EventoGateway;
+
+public class EventoRepositoryGateway implements EventoGateway {
+}
