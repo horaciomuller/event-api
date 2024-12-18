43322255281defd56bdc728cf0dd2d6547d4e753
Fiasco <contato@java10x.dev>
2024-12-18 14:30:55 +0000

    refactor(core): Injected the  dependency into  for proper gateway usage. Added a temporary  annotation in the core layer, acknowledging the need for future refactoring to align with clean architecture principles.

--- src/main/java/dev/java10x/EnvetClean/core/gateway/EventoGateway.java
+++ src/main/java/dev/java10x/EnvetClean/core/gateway/EventoGateway.java
@@ -1,4 +1,8 @@
 package dev.java10x.EnvetClean.core.gateway;
+import dev.java10x.EnvetClean.core.entities.Evento;
 
 public interface EventoGateway {
+
+    Evento criarEvento(Evento evento);
+
 }
--- src/main/java/dev/java10x/EnvetClean/core/usecases/CriarEventoUsecaseImpl.java
+++ src/main/java/dev/java10x/EnvetClean/core/usecases/CriarEventoUsecaseImpl.java
@@ -1,7 +1,16 @@
 package dev.java10x.EnvetClean.core.usecases;
 import dev.java10x.EnvetClean.core.entities.Evento;
+import dev.java10x.EnvetClean.core.gateway.EventoGateway;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CriarEventoUsecaseImpl implements CriarEventoUsecase {
+
+    private final EventoGateway eventoGateway;
+
+    public CriarEventoUsecaseImpl(EventoGateway eventoGateway) {
+        this.eventoGateway = eventoGateway;
+    }
 
     @Override
     public Evento execute(Evento evento) {
