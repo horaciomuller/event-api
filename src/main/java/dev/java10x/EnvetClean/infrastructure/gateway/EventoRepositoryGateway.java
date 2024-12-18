1f0bcbc87c3b757ff66632aa9f275b91d728e8c6
Fiasco <contato@java10x.dev>
2024-12-18 14:28:02 +0000

    feat:  to convert domain models into the entity expected by the repository. Injected the mapper into  to ensure proper entity conversion before interacting with the repository.

--- src/main/java/dev/java10x/EnvetClean/infrastructure/gateway/EventoRepositoryGateway.java
+++ src/main/java/dev/java10x/EnvetClean/infrastructure/gateway/EventoRepositoryGateway.java
@@ -1,5 +1,24 @@
 package dev.java10x.EnvetClean.infrastructure.gateway;
+import dev.java10x.EnvetClean.core.entities.Evento;
 import dev.java10x.EnvetClean.core.gateway.EventoGateway;
+import dev.java10x.EnvetClean.infrastructure.mapper.EventoEntityMapper;
+import dev.java10x.EnvetClean.infrastructure.persistence.EventoEntity;
+import dev.java10x.EnvetClean.infrastructure.persistence.EventoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class EventoRepositoryGateway implements EventoGateway {
+
+    private final EventoRepository eventoRepository;
+    private final EventoEntityMapper mapper;
+
+    @Override
+    public Evento criarEvento(Evento evento) {
+        EventoEntity entity = mapper.toEntity(evento);
+        EventoEntity novoEvento = eventoRepository.save(entity);
+        return mapper.toDomain(novoEvento);
+
+    }
 }
