a9fff63967b3c2712655cc3f2b590d5430b9c144
Fiasco <contato@java10x.dev>
2024-12-19 16:49:31 +0000

    feat: add exception handler for NotFoundEventException in ControllerExceptionHandler

--- src/main/java/dev/java10x/EventClean/infrastructure/exception/ControllerExceptionHandler.java
+++ src/main/java/dev/java10x/EventClean/infrastructure/exception/ControllerExceptionHandler.java
@@ -1,4 +1,5 @@
 package dev.java10x.EventClean.infrastructure.exception;
+import org.springframework.boot.autoconfigure.batch.BatchTaskExecutor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,7 +16,16 @@ public class ControllerExceptionHandler {
         response.put("Error: ", exception.getMessage());
         response.put("Meessage: ", "Please, insert a valid hashId for your event and try again");
     return new ResponseEntity<>(response, HttpStatus.CONFLICT);
-
     }
+
+    @ExceptionHandler(NotFoundEventException.class)
+    public ResponseEntity<Map<String, String>> handleNotFoundEventException(NotFoundEventException exception) {
+        Map<String, String> response = new HashMap<>();
+        response.put("Error: ", exception.getMessage());
+        response.put("Meessage: ", "The request event was not found, check the identifier and try again.");
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
+
+
 
 }
