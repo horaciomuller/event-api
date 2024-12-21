95702afc92a0d65a7101cab81abf6ca92db62bd3
Fiasco <contato@java10x.dev>
2024-12-21 18:41:16 +0000

    test: add unit tests for EventoController
    - Created EventoControllerTest.java to test the EventController
    - Modified EventCleanApplicationTests.java with additional test cases

--- src/test/java/dev/java10x/EventClean/EventCleanApplicationTests.java
+++ src/test/java/dev/java10x/EventClean/EventCleanApplicationTests.java
@@ -1,9 +1,13 @@
 package dev.java10x.EventClean;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 class EventCleanApplicationTests {
 
 	@Test
